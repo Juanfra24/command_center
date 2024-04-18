7e98ad73a07c2d8a732c82da3e6018f194bcd749
Juanfra24 <jfternicien@gmail.com>
2024-04-18 15:11:37 -0700

    initial commit

---
+++ lib/feature/splash/views/splash_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../../../config/theme/theme_manager.dart';
+import '../../../core/resource/widgets/custom_text_field.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        width: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Gap(20),
+            Text(
+              "Splash Screen",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            const Gap(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Toogle theme: ",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                Switch.adaptive(
+                    value: ThemeManage.isDarkModeActive(),
+                    onChanged: (val) {
+                      ThemeManage.changeThemeMode();
+                    }),
+              ],
+            ),
+            const Gap(20),
+            ElevatedButton(
+              onPressed: () {
+                int result = 5 ~/ 0;
+              },
+              style: ButtonStyle(
+                backgroundColor: ThemeManage.isDarkModeActive()
+                    ? const MaterialStatePropertyAll(
+                        Colors.orange,
+                      )
+                    : const MaterialStatePropertyAll(
+                        Colors.white,
+                      ),
+              ),
+              child: Text(
+                'Simulate Error',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            CustomTextField(),
+            CustomTextField(
+              isDropDown: true,
+              dropDownOptions: const [
+                "Test 1",
+                "Test 2",
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
